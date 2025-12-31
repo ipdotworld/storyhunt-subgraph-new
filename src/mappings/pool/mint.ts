563b8706db7d9ae1b1411e63f61b7702846c970e
Son Nguyen <son@ip.world>
2025-12-31 18:13:03 +0700

    test removing some entities

--- src/mappings/pool/mint.ts
+++ src/mappings/pool/mint.ts
@@ -7,11 +7,7 @@ import { getSubgraphConfig, SubgraphConfig } from '../../utils/chains'
 import { ONE_BI } from '../../utils/constants'
 import {
   updatePoolDayData,
-  updatePoolHourData,
-  updateTokenDayData,
-  updateTokenHourData,
   updateStoryHuntDayData,
-  updateTokenMinuteData,
 } from '../../utils/intervalUpdates'
 import { createTick } from '../../utils/tick'
 
@@ -128,13 +124,6 @@ export function handleMintHelper(event: MintEvent, subgraphConfig: SubgraphConfi
 
     updateStoryHuntDayData(event, factoryAddress)
     updatePoolDayData(event)
-    updatePoolHourData(event)
-    updateTokenDayData(token0 as Token, event)
-    updateTokenDayData(token1 as Token, event)
-    updateTokenHourData(token0 as Token, event)
-    updateTokenHourData(token1 as Token, event)
-    updateTokenMinuteData(token0 as Token, event)
-    updateTokenMinuteData(token1 as Token, event)
 
     token0.save()
     token1.save()
