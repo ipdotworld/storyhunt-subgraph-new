b90a209dd6bbb820e379395010acaaeae0a88b93
Sean <detectivekim1@gmail.com>
2026-02-14 03:57:39 +0900

    feat: add reward event indexing for Story Aeneid
    Add 7 new event handlers for Story Aeneid Testnet reward tracking:
    
    IPWorld (5 events):
    - HarvestDistributed: token/WETH fee distribution with computed wethToProtocol
    - AirdropClaimed: airdrop claim tracking per recipient
    - TreasuryFlushed: treasury token transfer tracking
    - Linked: IPA-to-token mapping for per-IP aggregation
    - LiquidityCollected: LP fee collection tracking
    
    IPOwnerVault (2 events):
    - ReleasedVested: vesting release tracking
    - EthDeposited: ETH deposit tracking
    
    Also updates:
    - IPOwnerVault proxy address to 0x276679F9e03d2E99350d407f964b5b42A3f01c73
    - ABI files with new event signatures from protocol contracts
    - Schema with 7 new entity types
    - subgraph.yaml with event handler registrations
    
    Ref: SPEC-SUBGRAPH-001

--- src/mappings/ipworld.ts
+++ src/mappings/ipworld.ts
@@ -1,6 +1,6 @@
 import { BigInt } from '@graphprotocol/graph-ts'
-import { TokenDeployed, Harvest } from '../types/IPWorld/IPWorld'
-import { TokenDeployment, HarvestEvent } from '../types/schema'
+import { TokenDeployed, Harvest, HarvestDistributed, AirdropClaimed, TreasuryFlushed, Linked, LiquidityCollected } from '../types/IPWorld/IPWorld'
+import { TokenDeployment, HarvestEvent, HarvestDistributedEvent, AirdropClaimEvent, TreasuryFlushEvent, IpTokenLink, LiquidityCollectedEvent } from '../types/schema'
 
 export function handleTokenDeployed(event: TokenDeployed): void {
   const tokenDeployment = new TokenDeployment(event.transaction.hash.toHexString() + '#' + event.logIndex.toString())
@@ -45,4 +45,86 @@ export function handleHarvest(event: Harvest): void {
   harvestEvent.transactionHash = event.transaction.hash.toHexString()
 
   harvestEvent.save()
+}
+
+export function handleHarvestDistributed(event: HarvestDistributed): void {
+  const id = event.transaction.hash.toHexString() + '#' + event.logIndex.toString()
+  const entity = new HarvestDistributedEvent(id)
+
+  entity.token = event.params.token.toHexString()
+  entity.tokenCollected = event.params.tokenCollected
+  entity.tokenToIpTreasury = event.params.tokenToIpTreasury
+  entity.tokenToAirdrop = event.params.tokenToUgc
+  entity.wethCollected = event.params.wethCollected
+  entity.wethToIpOwner = event.params.wethToIpOwner
+  entity.wethToBuyback = event.params.wethToBuyback
+  entity.wethToAirdrop = event.params.wethToUgc
+  // Computed: protocol treasury = total - ipOwner - buyback - airdrop
+  entity.wethToProtocol = event.params.wethCollected
+    .minus(event.params.wethToIpOwner)
+    .minus(event.params.wethToBuyback)
+    .minus(event.params.wethToUgc)
+  entity.blockNumber = event.block.number
+  entity.timestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash.toHexString()
+
+  entity.save()
+}
+
+export function handleAirdropClaimed(event: AirdropClaimed): void {
+  const id = event.transaction.hash.toHexString() + '#' + event.logIndex.toString()
+  const entity = new AirdropClaimEvent(id)
+
+  entity.token = event.params.token.toHexString()
+  entity.recipient = event.params.recipient.toHexString()
+  entity.tokenAmount = event.params.tokenAmount
+  entity.wethAmount = event.params.wethAmount
+  entity.blockNumber = event.block.number
+  entity.timestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash.toHexString()
+
+  entity.save()
+}
+
+export function handleTreasuryFlushed(event: TreasuryFlushed): void {
+  const id = event.transaction.hash.toHexString() + '#' + event.logIndex.toString()
+  const entity = new TreasuryFlushEvent(id)
+
+  entity.token = event.params.token.toHexString()
+  entity.treasury = event.params.treasury.toHexString()
+  entity.amount = event.params.amount
+  entity.blockNumber = event.block.number
+  entity.timestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash.toHexString()
+
+  entity.save()
+}
+
+export function handleLinked(event: Linked): void {
+  const id = event.params.ipaId.toHexString() + '-' + event.params.token.toHexString()
+  const entity = new IpTokenLink(id)
+
+  entity.ipaId = event.params.ipaId.toHexString()
+  entity.token = event.params.token.toHexString()
+  entity.blockNumber = event.block.number
+  entity.timestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash.toHexString()
+
+  entity.save()
+}
+
+export function handleLiquidityCollected(event: LiquidityCollected): void {
+  const id = event.transaction.hash.toHexString() + '#' + event.logIndex.toString()
+  const entity = new LiquidityCollectedEvent(id)
+
+  entity.pool = event.params.pool.toHexString()
+  entity.tickLower = BigInt.fromI32(event.params.tickLower)
+  entity.tickUpper = BigInt.fromI32(event.params.tickUpper)
+  entity.wethAmount = event.params.wethAmount
+  entity.tokenAmount = event.params.tokenAmount
+  entity.blockNumber = event.block.number
+  entity.timestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash.toHexString()
+
+  entity.save()
 }
